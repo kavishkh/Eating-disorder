05f40c8bcd5a3819aa28683d736036deefd1d621
kavishkh <kavishkhanna06@gmail.com>
2025-12-24 22:03:27 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
+import authRoutes from './routes/auth.routes.js';
 import moodRoutes from './routes/mood.js';
 import goalRoutes from './routes/goal.js';
 import chatRoutes from './routes/chat.js';
